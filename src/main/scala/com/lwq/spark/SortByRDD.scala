5a39890f04482c017127553600497a1deb813cb7
lwq <liwanqi@guazi.com>
2019-09-29 18:59:40 +0800

    新增SortByRDD案例和RepartitionRDD案例

---
+++ src/main/scala/com/lwq/spark/SortByRDD.scala
@@ -0,0 +1,28 @@
+package com.lwq.spark
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * @Author: Lwq
+  * @Date: 2019/9/28 19:08
+  * @Version 1.0
+  * @Describe
+  */
+object SortByRDD {
+  def main(args: Array[String]): Unit = {
+    //local模式
+    //创建sparkconf对象
+    //设定spark计算框架的运行环境
+    val conf = new SparkConf().setMaster("local[*]").setAppName("WordCount")
+
+    //创建spark上下文对象
+    val sc = new SparkContext(conf)
+
+    val listRDD: RDD[Int] = sc.makeRDD(List(2,13,4,23,14))
+    val sortByRDD: RDD[Int] = listRDD.sortBy(x=>x,false)
+    val sortByRDD1: RDD[Int] = listRDD.sortBy(x=>x%3,false)
+    sortByRDD.collect().foreach(println)
+    sortByRDD1.collect().foreach(println)
+  }
+}
